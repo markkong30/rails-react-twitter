8d332618c8f93edce6ad0e1fe6ec767db35d879b
markkong30 <markkong30@gmail.com>
2022-01-26 01:08:43 +0800

    commit

--- app/javascript/src/index.jsx
+++ app/javascript/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap'
 import Home from './home';
 import Tweets from './tweets';
 import User from './user';
